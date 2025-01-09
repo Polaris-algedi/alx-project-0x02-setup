6045b8d51b19cb4e1c467acf6d9e914dc869e8bf
Polaris-algedi <karchalmohammed@gmail.com>
2025-01-09 22:02:55 +0100

    Add and Use a Button Component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,22 @@
+import { ButtonProps } from "@/interfaces";
+export default function Button({ size, shape }: ButtonProps) {
+  let buttonSize: string;
+
+  switch (size) {
+    case "large":
+      buttonSize = "px-6 py-3 text-lg";
+      break;
+    case "medium":
+      buttonSize = "px-4 py-2 text-base";
+      break;
+    case "small":
+      buttonSize = "px-3 py-1 text-sm";
+      break;
+    default:
+      buttonSize = "px-4 py-2 text-base"; // default to medium if no size is provided
+      break;
+  }
+
+  const buttonStyle = `${buttonSize} ${shape} bg-blue-500 hover:bg-blue-700 text-white`;
+  return <button className={buttonStyle}>Click me</button>;
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/common/Button";
 import Header from "@/components/layout/Header";
 
 export default function About() {
@@ -5,6 +6,11 @@ export default function About() {
     <>
       <Header />
       <h1>About</h1>
+      <div className="flex gap-4 justify-center  items-center">
+        <Button shape="rounded-sm" size="small" />
+        <Button shape="rounded-md" size="medium" />
+        <Button shape="rounded-full" size="large" />
+      </div>
     </>
   );
 }
